0ae4f6a7f127ade2257272e4bca13b692d9f94bc
Jaylonnet <jaylonwingfield10@gmail.com>
2024-02-17 12:40:29 +0000

    Initial setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const gameWrapper = document.querySelector(".game-board");
+
+function createPlayer (name, letter) {
+    return {name, letter};
+};
+
+const Game = (function() {
+})();
+
+const Gameboard = (function() {
+    const board = [null, null, null, null, null, null, null, null, null];
+})();
